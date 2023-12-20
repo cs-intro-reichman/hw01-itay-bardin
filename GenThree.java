2def71893d5fd9a3d7ed9f028ecdb33f296778b7
Itay Bardin <154368714+itay-bardin@users.noreply.github.com>
2023-12-20 20:50:33 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -9,7 +9,7 @@ public class GenThree {
 		int rand1 = ((int)(Math.random() * (b - a) + a));
 		int rand2 = ((int)(Math.random() * (b - a) + a));
 		int rand3 = ((int)(Math.random() * (b - a) + a));
-		int minrRand = Math.min(Math.min(rand1,rand2),rand3);
+		int minRand = Math.min(Math.min(rand1,rand2),rand3);
 		System.out.println(rand1);
 		System.out.println(rand2);
 		System.out.println(rand3);
