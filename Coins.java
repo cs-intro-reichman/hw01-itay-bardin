8a8fb50d1c10fcfc3a515e412cf66160b5cd9ca5
Itay <itay.bardin@post.runi.ac.il>
2023-12-20 17:22:12 +0200

    Add awesome code for Homework 1

--- Coins.java
+++ Coins.java
@@ -4,6 +4,9 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		int cents = Integer.parseInt(args[0]);
+		int quarters = cents / 25;
+		int remainingCents = cents % 25;
+		System.out.println("Use " + quarters + " quarters and " + remainingCents + " cents");
 	}
 }
--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,15 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int rand1 = ((int)(Math.random() * (b - a) + a));
+		int rand2 = ((int)(Math.random() * (b - a) + a));
+		int rand3 = ((int)(Math.random() * (b - a) + a));
+		int minrRand = Math.min(Math.min(rand1,rand2),rand3);
+		System.out.println(rand1);
+		System.out.println(rand2);
+		System.out.println(rand3);
+		System.out.println("The minimal generated number is " + minRand);
 	}
 }
